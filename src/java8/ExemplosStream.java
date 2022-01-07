34cc2750a2dd9d14db6d61e86a4f12b37280d5c7
Salatiel dos Reis <salatieldrg@gmail.com>
2022-01-06 21:29:53 -0300

    trabalhando com streams

--- src/java8/ExemplosStream.java
+++ src/java8/ExemplosStream.java
@@ -1,8 +1,13 @@
 package java8;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ExemplosStream {
@@ -28,5 +33,36 @@ public class ExemplosStream {
 				   .filter(c -> c.getAlunos() > 100)
 				   .mapToInt(Curso::getAlunos)
 				   .sum();
+		
+//		Optional<Curso> optionalCurso = cursos.stream()
+//			.filter(c -> c.getAlunos() >= 100)
+//			.findAny();
+		
+//		Curso curso = optionalCurso.get();
+//		Curso curso = optionalCurso.orElse(null);
+//		optionalCurso.ifPresent(c -> System.out.println(c.getNome()));
+		
+		cursos.stream()
+				.filter(c -> c.getAlunos() >= 100)
+				.findAny()
+				.ifPresent(c -> System.out.println(c.getNome()));
+		
+		List<Curso> listaCursos = cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.collect(Collectors.toList());
+		
+		listaCursos.forEach(c -> System.out.println(c.getNome()));
+		
+		Map<String, Integer> mapCursos = cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.collect(Collectors.toMap(c -> c.getNome(), c -> c.getAlunos()));
+		
+		mapCursos.forEach((nome, valor) -> System.out.println("o nome é" + nome + ", e o valor é " + valor));
+		
+		cursos.stream()
+			.mapToDouble(c -> c.getAlunos())
+			.average().ifPresent(average -> System.out.println(average));
+		
+		
 	}
 }
